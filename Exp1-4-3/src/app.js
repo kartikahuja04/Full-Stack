6930142503e11b6d0e7b741fbca73a26d7f2dd3b
Kartik Ahuja <kartikahuja04jan@gmail.com>
2026-03-04 18:04:21 +0530

    Serve index.html at root to avoid 'Cannot GET /' on deploy

--- Exp1-4-3/src/app.js
+++ Exp1-4-3/src/app.js
@@ -1,9 +1,24 @@
 import express from 'express';
 import bookingRoutes from './modules/booking/booking.route.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use('/api', bookingRoutes);
+
+// Serve the root index.html from project root so visiting '/' is friendly
+app.get('/', (req, res) => {
+	const indexPath = path.resolve(__dirname, '..', 'index.html');
+	res.sendFile(indexPath, (err) => {
+		if (err) {
+			res.status(500).send('Error loading page');
+		}
+	});
+});
 
 export default app;
